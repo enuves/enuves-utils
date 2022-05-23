847eeefbbb0228f0a95c45cea86f6e9956e54f00
Welkson Ramos <welksonr@hotmail.com>
2022-05-23 16:32:35 -0300

    chore: validate strings in JSON

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+
 export { capitalize } from "./lib/capitalize.js";
+export { isJsonString } from "./lib/is-json-string.js";
 export { prettyBytes } from "./lib/pretty-bytes.js";
 export { sortArray } from "./lib/sort-array.js";
 export { uniqueArray } from "./lib/unique-array.js";
