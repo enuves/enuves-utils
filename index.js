d6dfccbf11d7f3bb6b8f2062ae80159a43c6b7ed
Welkson Ramos <welksonr@hotmail.com>
2022-05-23 13:14:05 -0300

    chore: sort a object array by property

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { capitalize } from "./lib/capitalize.js";
 export { prettyBytes } from "./lib/pretty-bytes.js";
+export { sortArray } from "./lib/sort-array.js";
 export { uniqueArray } from "./lib/unique-array.js";
